659f1f21dc76bbc47ee37cc07bccd53c52c5a046
dudcks0994 <dudcks0994@gmail.com>
2025-03-03 00:01:58 +0900

    reinit

--- Airdrop/src/DataLayout.sol
+++ Airdrop/src/DataLayout.sol
@@ -5,8 +5,8 @@ contract DatalayoutLibrary{
     address token_addr;
     address owner_addr;
     bytes32 merkle_root;
-    uint    expired_at;
-    uint    total_supply;
+    uint    public expired_at;
+    uint    public total_supply;
     mapping(address => bool) has_claimed;
     enum State {INIT, START, PAUSED} 
     State state;
